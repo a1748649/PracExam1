7229a8a88583289997fe38162bb1a1329e931233
a1748649 <95258325+a1748649@users.noreply.github.com>
2022-08-10 13:47:34 +0930

    Add files via upload

---
+++ function-2-3.cpp
@@ -0,0 +1,28 @@
+int palindrome_sum(int integers[], int length){
+    int result;
+
+    if (is_array_palindrome(integers, length)){
+        result = sum_integers(integers, length);
+        return result;
+    }
+    else{
+        return -2;
+    }
+}
+
+bool is_array_palindrome(int integers[], int length){
+    for (int i  = 0; i < length; i++){
+        int x = 2;
+    }
+    return true;
+}
+
+int sum_integers(int integers[], int length){
+    int result = 0;
+
+    for(int i = 0; i < length; i++){
+        result = result + integers[i];
+    }
+
+    return result;
+}
---
+++ main-2-3.cpp
@@ -0,0 +1,11 @@
+#include "function-2-3.cpp"
+
+int main(){
+    int result;
+    int integers = [1,2,3,4];
+    int length = 4;
+
+    result = palindrome_sum(integers, length);
+
+    return 0;
+}
