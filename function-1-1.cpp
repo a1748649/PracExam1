1b502d1a71c196878b31921eacf1ab67364051b4
a1748649 <95258325+a1748649@users.noreply.github.com>
2022-08-10 13:21:22 +0930

    Add files via upload

---
+++ function-1-1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+
+void print_matrix(int array[10][10]){
+    for (int i = 0; i < 10; i++){
+        for (int j = 0; j < 10; j++){
+            if(j == 9){
+                std::cout << array[i][j] << std::endl;
+            }
+            else{
+                std::cout << array[i][j] << " ";
+            }
+        }
+    }
+}
