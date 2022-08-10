2f604b433e9f91d6f6fb26140d3537c3d3573775
a1748649 <95258325+a1748649@users.noreply.github.com>
2022-08-10 13:30:58 +0930

    Add files via upload

---
+++ function-2-2.cpp
@@ -0,0 +1,9 @@
+int bin_to_int(int binary_digits[], int number_of_digits){
+    number = 0;
+    for(int i = 0; i < number_of_digits; i++){
+        if(binary_digits[i] == 1){
+            number = number + 2**(number_of_digits - i - 1)
+        }
+    }
+    return number;
+}
---
+++ main-2-2.cpp
@@ -0,0 +1,11 @@
+// int bin_to_int(int binary_digits[], int number_of_digits);
+#include "function-2-2.cpp"
+
+int main(){
+    int binary[] = {1,0,1,0,1};
+    int num_digits = 5;
+
+    result = bin_to_int(binary, num_digits);
+
+    return 0;
+}
