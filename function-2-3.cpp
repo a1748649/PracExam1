d532a0e0bb07dad31ba6b9fcfe50ee97de4bccdd
a1748649 <95258325+a1748649@users.noreply.github.com>
2022-08-10 13:49:08 +0930

    Add files via upload

--- function-2-3.cpp
+++ function-2-3.cpp
@@ -1,15 +1,3 @@
-int palindrome_sum(int integers[], int length){
-    int result;
-
-    if (is_array_palindrome(integers, length)){
-        result = sum_integers(integers, length);
-        return result;
-    }
-    else{
-        return -2;
-    }
-}
-
 bool is_array_palindrome(int integers[], int length){
     for (int i  = 0; i < length; i++){
         int x = 2;
@@ -25,4 +13,16 @@ int sum_integers(int integers[], int length){
     }
 
     return result;
+}
+
+int palindrome_sum(int integers[], int length){
+    int result;
+
+    if (is_array_palindrome(integers, length)){
+        result = sum_integers(integers, length);
+        return result;
+    }
+    else{
+        return -2;
+    }
 }
